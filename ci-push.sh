0da4cd48ca222839f7be7b8965bbe050728595b1
Andraz Vrhovec <andraz@koofr.net>
2020-03-22 18:44:52 +0100

    wip

--- ci-push.sh
+++ ci-push.sh
@@ -5,6 +5,7 @@ REPO="github.com/slo-covid-19/data"
 # copy new data into repo
 
 git add csv/*
+git checkout master
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git remote add upstream https://${GITHUB_TOKEN}@${REPO}.git > /dev/null 2>&1
 git push --quiet upstream master
