c93b5d0c7d2354faf6a96619a02ee158e006d2e9
Andraz Vrhovec <andraz@koofr.net>
2020-05-13 09:31:44 +0200

    reorder git commands

--- ci-push.sh
+++ ci-push.sh
@@ -4,8 +4,8 @@ REPO="github.com/sledilnik/data"
 
 # copy new data into repo
 
-git add csv/*
 git checkout master
+git add csv/*
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git remote add upstream https://${GITHUB_TOKEN}@${REPO}.git > /dev/null 2>&1
 git push --quiet upstream master
