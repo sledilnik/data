e8f74f99b18a4f31dfeb961c7eceae64ebbe71b1
Stefan Baebler <sbaebler@outbrain.com>
2020-09-30 13:01:02 +0200

    fix 2

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 # Podatki Ministrstva za Izobraževanje Znanost in Šport (MIZŠ) RS
 # API docs at http://api.mizs.si/api_dokumentacija.html
-curl -s http://api.mizs.si/stats/ | jq > json/mizs-stats.json
+curl -s http://api.mizs.si/stats/ | jq '.' > json/mizs-stats.json
