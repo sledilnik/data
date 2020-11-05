4a2965a692f07e9cdf190dc691cd573d2391f54c
Stefan Baebler <sbaebler@outbrain.com>
2020-11-05 16:34:38 +0100

    Ignore TLS certificate errors for now

--- ostanizdrav/update-ostanizdrav.sh
+++ ostanizdrav/update-ostanizdrav.sh
@@ -11,6 +11,6 @@ curl -s https://ostanizdrav.sledilnik.org/plots/usersByCount.json | jq '.' > ctt
 
 # https://podatki.gov.si/dataset/statisticni-podatki-aplikacije-ostanizdrav
 mkdir -p podatki.gov.si || true
-curl https://podatki.gov.si/dataset/d3e718b3-01f0-40bd-93b0-b56d3bb4e97f/resource/f8711982-df42-48e6-9c07-92fcb89cb15c/download/prenosiandroid.csv -o podatki.gov.si/prenosiandroid.csv
-curl https://podatki.gov.si/dataset/d3e718b3-01f0-40bd-93b0-b56d3bb4e97f/resource/cf4601a8-edb0-4c3e-8646-a8d9a4094375/download/iosprenosi.csv -o podatki.gov.si/iosprenosi.csv
-curl https://podatki.gov.si/dataset/d3e718b3-01f0-40bd-93b0-b56d3bb4e97f/resource/406b4306-cf45-4d49-b44e-48c8b4c1de78/download/tanstatistika.csv -o podatki.gov.si/tanstatistika.csv
+curl --insecure https://podatki.gov.si/dataset/d3e718b3-01f0-40bd-93b0-b56d3bb4e97f/resource/f8711982-df42-48e6-9c07-92fcb89cb15c/download/prenosiandroid.csv -o podatki.gov.si/prenosiandroid.csv
+curl --insecure https://podatki.gov.si/dataset/d3e718b3-01f0-40bd-93b0-b56d3bb4e97f/resource/cf4601a8-edb0-4c3e-8646-a8d9a4094375/download/iosprenosi.csv -o podatki.gov.si/iosprenosi.csv
+curl --insecure https://podatki.gov.si/dataset/d3e718b3-01f0-40bd-93b0-b56d3bb4e97f/resource/406b4306-cf45-4d49-b44e-48c8b4c1de78/download/tanstatistika.csv -o podatki.gov.si/tanstatistika.csv
