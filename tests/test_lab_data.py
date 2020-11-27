00a1445a52a270465325e09c376deb012190eb33
Roman Luštrik <roman.lustrik@biolitika.si>
2020-11-27 18:02:49 +0100

    Replace 0 with NaN in final output of lab-tests.csv

--- tests/test_lab_data.py
+++ tests/test_lab_data.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from unittest import TestCase
 
+import numpy as np
 import pandas as pd
 from lab_tests.parse_daily_tests import parse_daily_tests
 
@@ -18,6 +19,12 @@ class TestParsingTestsLab(TestCase):
         )
         xy = pd.read_csv(self.xy_file)
         self.assertEqual(list(xy.date)[-1], "2020-11-20")
+
+        # Make sure zeros are replaced with NaN. test.positive column should have
+        # NaN by default, but .todate column zeros are replaced with NaN post
+        # festum.
+        self.assertTrue(np.isnan(list(xy["tests.positive"])[0]))
+        self.assertTrue(np.isnan(list(xy["tests.positive.todate"])[0]))
 
         self.xy_file.unlink()
         self.xy_timestamp.unlink()
