6448b6195891313e932060b0348bc6aae70124c6
Stefan Baebler <sbaebler@outbrain.com>
2020-12-14 08:07:21 +0100

    Scrape also list of devices, compatible with the app
    From https://www.gov.si/assets/devices.csv

--- ostanizdrav/update-ostanizdrav.sh
+++ ostanizdrav/update-ostanizdrav.sh
@@ -5,9 +5,10 @@ curl -s https://ostanizdrav.sledilnik.org/plots/data.json         | jq '.' > ctt
 curl -s https://ostanizdrav.sledilnik.org/plots/dataByRisk.json   | jq '.' > ctt/dataByRisk.json
 curl -s https://ostanizdrav.sledilnik.org/plots/usersByCount.json | jq '.' > ctt/usersByCount.json
  
-# mkdir -p gov.si || true
+mkdir -p gov.si || true
 # curl https://www.gov.si/teme/koronavirus-sars-cov-2/mobilna-aplikacija-ostanizdrav/element/83674/izvoz.csv -o gov.si/app-downloads.csv
 # curl https://www.gov.si/teme/koronavirus-sars-cov-2/mobilna-aplikacija-ostanizdrav/element/83668/izvoz.csv -o gov.si/tan-issued.csv
+curl https://www.gov.si/assets/devices.csv -o gov.si/devices.csv
 
 # https://podatki.gov.si/dataset/statisticni-podatki-aplikacije-ostanizdrav
 mkdir -p podatki.gov.si || true
