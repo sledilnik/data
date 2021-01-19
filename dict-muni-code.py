66c499139bcfe429b046d35548f40a7a7f5f2842
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2021-01-19 18:56:24 +0100

    Simple script to convert muni dict to code dict

---
+++ dict-muni-code.py
@@ -0,0 +1,4 @@
+import csv
+import pandas as pd
+df1=pd.read_csv("csv/dict-municipality.csv", index_col='id') [[ "name","iso_code","population" ]]
+df1.to_csv("csv/dict-municipality-code.csv", quoting=csv.QUOTE_NONNUMERIC, index_label='id')
