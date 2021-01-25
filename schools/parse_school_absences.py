0737a63eb5bf99d180a294b5ae0d21d088568339
Andraz Vrhovec <andraz@koofr.net>
2021-01-25 14:23:12 +0100

    MIZS schools data parser

---
+++ schools/parse_school_absences.py
@@ -0,0 +1,172 @@
+#! /bin/python
+
+from operator import itemgetter
+from datetime import datetime
+import codecs
+import csv
+import re
+import requests
+import io
+
+# KAT_MAT/KATEGORIJA (Iz Šifrant_kategorij.xslx)
+SCHOOL_TYPES = {
+    "1010": "PV",  # Vrtec
+    "2010": "OŠ",  # Osnovna šola
+    "2020": "OŠPP",  # Osnovna šola za učence s posebnimi potrebami
+    "2030": "GŠ",  # Glasbena šola
+    "3010": "SŠ",  # Srednja šola
+    "3020": "DD",  # Dijaški dom
+    "4010": "ZAV",  # Zavodi za otroke in mladostnike s posebnimi potrebami
+    "5040": "VSS",  # Višja strokovna šola
+}
+
+# School unit (ZAVNAZ) fixes (parts to remove)
+UNIT_REMOVE = [
+    "Osnovna šola Antona Globočnika",
+    "Osnovna šola Destrnik",
+    "Osnovna šola Frana Kocbeka Gornji grad",
+    "Osnovna šola Miroslava Vilharja",
+    "Osnovna šola Ob Dravinji",
+    "Osnovna šola Pohorskega odreda Slov. Bistrica",
+    "Osnovna šola Sava Kladnika",
+    "Osnovna šola dr. Janeza Mencingerja Boh. Bistrica",
+    "VIZ II.OŠ Rogaška Slatina",
+]
+
+# School unit (Zavnaz) manual fixes
+UNIT_FIXES = {}
+
+
+def reformat_unit(row):
+    """
+        Remove the main school from the school unit field if present.
+        We just want the unit name there.
+    """
+
+    # if the unit has the same id as the school
+    if row[0] == row[2]:
+        row[3] = ""
+
+    # if unit in school
+    if row[1] in row[3]:
+        # remove school from unit
+        row[3] = row[3].replace(row[1], "")
+        # capitalize first letter since it might be lowercase now
+        if len(row[3]) >= 2:
+            row[3] = row[3][0].upper() + row[3][1:]
+
+    # fix some additional mistakes
+    for mistake in UNIT_REMOVE:
+        if row[3].startswith(mistake):
+            row[3] = row[3].replace(mistake, "")
+    for mistake, fix in UNIT_FIXES.items():
+        if row[3] == mistake:
+            row[3] == fix
+
+    # trim
+    if row[3].startswith(", "):
+        row[3] = row[3].replace(", ", "", 1)
+    row[3] = row[3].strip(" ")
+
+
+def reformat_dates(date_columns, row):
+    """
+    Reformat the dates from a human-readable d.m.Y form into
+    the standard YMD form.
+    """
+    for i in date_columns:
+        dmy = row[i]
+        match = re.match(r"0?(\d+)\.0?(\d+)\.(\d+)", dmy)
+        if not match:
+            return
+
+        y = int(match.group(3))
+        m = int(match.group(2))
+        d = int(match.group(1))
+        date = datetime(y, m, d)
+        row[i] = date.strftime("%Y-%m-%d")
+
+
+def parse_csv(url):
+    """
+    Parse the MIZS Covid-19 pupuls (Uc.csv) and school employees (Zap.csv).
+    """
+
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    rows = []
+
+    reader = csv.reader(io.StringIO(resp.text), delimiter=",")
+
+    date_columns = range(8, 11)
+    for row in reader:
+        if not row or row[0].startswith("ZAVSIF"):
+            continue
+
+        reformat_unit(row)
+        reformat_dates(date_columns, row)
+        rows.append(row)
+
+    # sort by reporting date (the last date column)
+    rows = sorted(rows, key=itemgetter(date_columns[-1]))
+    return rows
+
+
+def merge_confirmed_infections():
+    """
+    Merge confirmed atendee and employee absences by date, school, unit"
+    """
+    # merge and sort
+    attendees = parse_csv("https://raw.githubusercontent.com/GK-MIZS/covid/main/ucenci.csv")
+    employees = parse_csv("https://raw.githubusercontent.com/GK-MIZS/covid/main/zaposleni.csv")
+    absences = sorted(attendees + employees, key=itemgetter(10, 7, 1, 3))
+
+    # transform
+    new = []
+    for a in absences:
+        school_type = SCHOOL_TYPES.get(a[5], "N/A")  # KATEGORIJA
+        municipality = a[7]  # OBCINA
+        school_id = a[0]
+        school = a[1]  # ZAVMATNAZ
+        unit = a[3]  # ZAVNAZ
+        subunit = a[12]  # OBDOBJE
+        description = a[14]  # VZROK
+
+        row = [
+            a[10],
+            a[8],
+            a[9],
+            municipality,
+            school_type,
+            school_id,
+            school,
+            unit,
+            subunit,
+            description,
+        ]
+        new.append(row)
+
+    with codecs.open("csv/school-absences.csv", "w", "utf-8") as csvfile:
+        csvwriter = csv.writer(
+            csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        header = [
+            "date",
+            "absent.from",
+            "absent.to",
+            "municipality",
+            "type",
+            "school_id",
+            "school",
+            "unit",
+            "subunit",
+            "reason",
+        ]
+        csvwriter.writerow(header)
+        for row in new:
+            csvwriter.writerow(row)
+
+
+if __name__ == "__main__":
+    merge_confirmed_infections()
---
+++ schools/parse_school_cases.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+"""
+  Prepare daily covid-19 case summaries for schools
+"""
+
+import codecs
+import json
+import csv
+import requests
+
+def parse_api():
+    header = [
+        "date",
+        "cases.confirmed.todate",
+        "cases.active",
+        "quarantine.confirmed.todate",
+        "quarantine.active",
+        "cases.attendees.confirmed.todate",
+        "cases.attendees.active",
+        "cases.employees.confirmed.todate",
+        "cases.employees.active",
+    ]
+    rows = []
+    rows.append(header)
+
+    resp = requests.get('https://raw.githubusercontent.com/GK-MIZS/covid/main/api.json')
+    resp.raise_for_status()
+
+    j = resp.json()
+
+    updates = j[1:]
+
+    cases_confirmed_todate = 0
+    quarantines_confirmed_todate = 0
+    cases_confirmed_todate_attendees = 0
+    cases_confirmed_todate_employees = 0
+    for u in updates:
+        cases_confirmed = (
+            u["ucenci_okuzbe_nove"]["Osnovna_sola"]
+            + u["ucenci_okuzbe_nove"]["Osnovna_sola_s_prilagojenim_programom"]
+            + u["zaposleni_okuzbe_nove"]["Osnovna_sola"]
+            + u["zaposleni_okuzbe_nove"]["Osnovna_sola_s_prilagojenim_programom"]
+        )
+        cases_confirmed_todate += cases_confirmed
+        cases_active = (
+            u["ucenci_okuzbe_aktivne"]["Osnovna_sola"]
+            + u["ucenci_okuzbe_aktivne"]["Osnovna_sola_s_prilagojenim_programom"]
+            + u["zaposleni_okuzbe_aktivne"]["Osnovna_sola"]
+            + u["zaposleni_okuzbe_aktivne"]["Osnovna_sola_s_prilagojenim_programom"]
+        )
+
+        quarantines_confirmed = (
+            u["ucenci_v_karanteni_novi"]["Osnovna_sola"]
+            + u["ucenci_v_karanteni_novi"]["Osnovna_sola_s_prilagojenim_programom"]
+        )
+        quarantines_confirmed_todate += quarantines_confirmed
+        quarantines_active = (
+            u["ucenci_v_karanteni_aktivne"]["Osnovna_sola"]
+            + u["ucenci_v_karanteni_aktivne"]["Osnovna_sola_s_prilagojenim_programom"]
+        )
+
+        cases_confirmed_attendees = (
+            u["ucenci_okuzbe_nove"]["Osnovna_sola"]
+            + u["ucenci_okuzbe_nove"]["Osnovna_sola_s_prilagojenim_programom"]
+        )
+        cases_confirmed_todate_attendees += cases_confirmed_attendees
+        cases_active_attendees = (
+            u["ucenci_okuzbe_aktivne"]["Osnovna_sola"]
+            + u["ucenci_okuzbe_aktivne"]["Osnovna_sola_s_prilagojenim_programom"]
+        )
+
+        cases_confirmed_employees = (
+            u["zaposleni_okuzbe_nove"]["Osnovna_sola"]
+            + u["zaposleni_okuzbe_nove"]["Osnovna_sola_s_prilagojenim_programom"]
+        )
+        cases_confirmed_todate_employees += cases_confirmed_employees
+        cases_active_employees = (
+            u["zaposleni_okuzbe_aktivne"]["Osnovna_sola"]
+            + u["zaposleni_okuzbe_aktivne"]["Osnovna_sola_s_prilagojenim_programom"]
+        )
+
+        rows.append(
+            [
+                "{}-{}-{}".format(u["year"], u["month"], u["day"]),
+                cases_confirmed_todate,
+                cases_active,
+                quarantines_confirmed_todate,
+                quarantines_active,
+                cases_confirmed_todate_attendees,
+                cases_active_attendees,
+                cases_confirmed_todate_employees,
+                cases_active_employees,
+            ]
+        )
+
+    with codecs.open("csv/schools-cases.csv", "w", "utf-8") as f:
+        csvwriter = csv.writer(
+            f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        csvwriter.writerows(rows)
+
+
+if __name__ == "__main__":
+    parse_api()
