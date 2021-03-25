aa1d2b75f30fb48411d8e855f8ea7fd01b052133
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2021-03-25 13:32:48 +0100

    NIJZ removed vaccinations - renamed all tables

--- transform/nijz_daily.py
+++ transform/nijz_daily.py
@@ -54,7 +54,7 @@ def get_municipality_header(municipality: str):
     return f'region.{region}.{id_}'
 
 
-df = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 3', engine='openpyxl', skiprows=[0, 2], skipfooter=1).transpose()[:-1]
+df = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 2', engine='openpyxl', skiprows=[0, 2], skipfooter=1).transpose()[:-1]
 df.columns = df.iloc[0]  # sets the header to municipality name instead of having a zero-based index for header
 df = df[1:]  # now that municipality is set for the header, we can delete it from the value matrix
 
@@ -95,7 +95,7 @@ with open(municipality_deceased_csv_path, 'w', newline='', encoding='utf-8') as 
 write_timestamp_file(filename=municipality_deceased_csv_path, old_hash=old_hash)
 
 # --- region-confirmed.csv | region-active.csv ---
-df = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 4', engine='openpyxl', skiprows=[0, 2])[:-1]
+df = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 3', engine='openpyxl', skiprows=[0, 2])[:-1]
 df.drop(['SKUPAJ'], inplace=True, axis=1)  # axis=1 means columns
 
 
@@ -154,7 +154,7 @@ with open(region_deceased_csv_path, 'w', newline='', encoding='utf-8') as csvfil
 write_timestamp_file(filename=region_deceased_csv_path, old_hash=old_hash)
 
 # --- age-confirmed.csv ---
-df = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 5', engine='openpyxl', skiprows=[1, 2, 3])[:-1]
+df = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 4', engine='openpyxl', skiprows=[1, 2, 3])[:-1]
 df.rename(columns={'Dnevno število potrjenih primerov po spolu in starostnih skupinah': 'date'}, inplace=True)
 df.set_index('date', inplace=True)
 df.rename(mapper=lambda x: datetime.strptime(x, '%d.%m.%Y'), axis='rows', inplace=True)
@@ -182,7 +182,7 @@ df_1['cases.active'] = df_1['cases.confirmed'].rolling(window=14).sum().astype('
 df_1['cases.closed.todate'] = df_1['cases.confirmed.todate'] - df_1['cases.active']
 df_1 = df_1.join(df_cases[['cases.recovered.todate']])
 
-df_6 = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 6', engine='openpyxl', skiprows=[0, 2], skipfooter=2) \
+df_6 = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 5', engine='openpyxl', skiprows=[0, 2], skipfooter=2) \
     .rename(mapper={'Datum izvida': 'date', 'Oskrbovanci': 'cases.rh.occupant.confirmed'}, axis='columns').set_index('date') \
     .rename(mapper=lambda x: datetime.strptime(x, '%d.%m.%Y'), axis='rows')[['cases.rh.occupant.confirmed']]
 df_6['cases.rh.occupant.confirmed.todate'] = df_6['cases.rh.occupant.confirmed'].cumsum()
@@ -200,23 +200,23 @@ for date in df_cases.index.difference(df_joined.index):  # do not delete latest 
 export_dataframe_to_csv(name='cases', dataframe=df_joined)
 
 # --- vaccination-administered.csv ---
-df_2 = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 2', engine='openpyxl', skiprows=[0], skipfooter=1) \
-    .rename(mapper={
-        'Datum podatkov': 'date',
-        'Število cepljenih oseb s prvim odmerkom': 'vaccination.administered',
-        'Število cepljenih oseb z drugim odmerkom': 'vaccination.administered2nd'
-    }, axis='columns').set_index('date')
-#df_2 = df_1[df_1.index.notnull()]  # drop non-indexed rows (where NaN is a part of the index)
-df_2.drop('SKUPAJ', axis='rows', inplace=True)
-df_2 = df_2.rename(mapper=lambda x: datetime.strptime(x, '%d.%m.%Y'), axis='rows')
-df_2['vaccination.administered.todate'] = df_2['vaccination.administered'].cumsum()
-df_2['vaccination.administered2nd.todate'] = df_2['vaccination.administered2nd'].cumsum()
-df_2['vaccination.used.todate'] = df_2['vaccination.administered.todate'] + df_2['vaccination.administered2nd.todate']
-
-df_2 = df_2.reindex([
-    'vaccination.administered', 'vaccination.administered.todate',
-    'vaccination.administered2nd', 'vaccination.administered2nd.todate',
-    'vaccination.used.todate'
-    ], axis='columns')
-
-export_dataframe_to_csv(name='vaccination-administered', dataframe=df_2)
+#df_2 = pd.read_excel(io=SOURCE_FILE, sheet_name='Tabela 2', engine='openpyxl', skiprows=[0], skipfooter=1) \
+#    .rename(mapper={
+#        'Datum podatkov': 'date',
+#        'Število cepljenih oseb s prvim odmerkom': 'vaccination.administered',
+#        'Število cepljenih oseb z drugim odmerkom': 'vaccination.administered2nd'
+#    }, axis='columns').set_index('date')
+##df_2 = df_1[df_1.index.notnull()]  # drop non-indexed rows (where NaN is a part of the index)
+#df_2.drop('SKUPAJ', axis='rows', inplace=True)
+#df_2 = df_2.rename(mapper=lambda x: datetime.strptime(x, '%d.%m.%Y'), axis='rows')
+#df_2['vaccination.administered.todate'] = df_2['vaccination.administered'].cumsum()
+#df_2['vaccination.administered2nd.todate'] = df_2['vaccination.administered2nd'].cumsum()
+#df_2['vaccination.used.todate'] = df_2['vaccination.administered.todate'] + df_2['vaccination.administered2nd.todate']
+#
+#df_2 = df_2.reindex([
+#    'vaccination.administered', 'vaccination.administered.todate',
+#    'vaccination.administered2nd', 'vaccination.administered2nd.todate',
+#    'vaccination.used.todate'
+#    ], axis='columns')
+#
+#export_dataframe_to_csv(name='vaccination-administered', dataframe=df_2)
