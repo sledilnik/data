476514405f88c42bc8f8f12d38b745e2c89ade76
Stefan Baebler <stefan.baebler@gmail.com>
2021-05-03 20:42:49 +0200

    Vaccinations per municipalities: share -> percent

--- update_vaccination.py
+++ update_vaccination.py
@@ -257,13 +257,13 @@ def import_nijz_dash_vacc_by_municipalities():
         munId=mun.to_records()[0].id
         municipalities.loc[munId, 'population_nijz'] = row.population
         municipalities.loc[munId, '1st.todate'] = row.dose1
-        municipalities.loc[munId, '1st.share.todate'] = round(row.share1, 5)
+        municipalities.loc[munId, '1st.percent.todate'] = round(row.share1 * 100, 3)
         municipalities.loc[munId, '2nd.todate'] = row.dose2
-        municipalities.loc[munId, '2nd.share.todate'] = round(row.share2, 5)
+        municipalities.loc[munId, '2nd.percent.todate'] = round(row.share2 * 100, 3)
 
 
     # trim down extra columns
-    municipalities = municipalities[['region', 'iso_code', 'name', 'population', '1st.todate', '1st.share.todate', '2nd.todate', '2nd.share.todate']]
+    municipalities = municipalities[['region', 'iso_code', 'name', 'population', '1st.todate', '1st.percent.todate', '2nd.todate', '2nd.percent.todate']]
     municipalities['1st.todate']=municipalities['1st.todate'].astype('Int64')
     municipalities['2nd.todate']=municipalities['2nd.todate'].astype('Int64')
     municipalities.dropna(thresh=4, inplace=True)
@@ -279,9 +279,9 @@ def import_nijz_dash_vacc_by_municipalities():
         fieldPrefix=f'region.{m["region"]}.{id}.'
         # today_data[f'{fieldPrefix}population'] = m["population"]
         today_data[f'{fieldPrefix}1st.todate'] = m["1st.todate"]
-        # today_data[f'{fieldPrefix}1st.share.todate'] = m["1st.share.todate"]
+        # today_data[f'{fieldPrefix}1st.percent.todate'] = m["1st.percent.todate"]
         today_data[f'{fieldPrefix}2nd.todate'] = m["2nd.todate"]
-        # today_data[f'{fieldPrefix}2nd.share.todate'] = m["2nd.share.todate"]
+        # today_data[f'{fieldPrefix}2nd.percent.todate'] = m["2nd.percent.todate"]
 
     df_today = pd.DataFrame([today_data], index=[datetime.date.today()])
     df_today.index.name = 'date'
