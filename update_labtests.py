6678df77ac253865ee2624d07484a02283acdc7d
Stefan Baebler <stefan.baebler@gmail.com>
2021-06-20 10:19:22 +0200

    labtest: recalculate .todate fields

--- update_labtests.py
+++ update_labtests.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from datetime import timedelta
 import time
 import pandas as pd
 import cepimose
@@ -15,21 +14,13 @@ def import_nijz_dash_labtests():
     df_existing = pd.read_csv(filenameByDay, index_col='date', parse_dates=['date'])
     print(df_existing)
     d = cepimose.lab_end_timestamp()
-    earlierdate = d - timedelta(days=1)
-    print(d, earlierdate)
-
+    print(f"Adding/updating lab test data for {d.date()}")
 
     # copy last row structure, with None values:
     day_data = dict.fromkeys(df_existing.tail(1), None)
-    
     day_data['tests.regular.performed'] = cepimose.lab_PCR_tests_performed() 
-    day_data['tests.regular.performed.todate'] = cepimose.lab_PCR_total_tests_performed()
-
     day_data['tests.regular.positive'] = cepimose.lab_cases_confirmed()
-    day_data['tests.regular.positive.todate'] = cepimose.lab_cases_total_confirmed()
-
     day_data['tests.hagt.performed'] = cepimose.lab_HAT_tests_performed()
-    day_data['tests.hagt.performed.todate'] = cepimose.lab_HAT_total_tests_performed()
 
     day_data['tests.performed'] = day_data['tests.regular.performed']
     day_data['tests.positive'] = day_data['tests.regular.positive']
@@ -50,6 +41,19 @@ def import_nijz_dash_labtests():
     print(df_day_data)
 
     df_updated = df_day_data.combine_first(df_existing).fillna(0).round().replace({0: None}).astype('Int64')
+
+    #recalculate .todate fields
+    df_updated['tests.regular.performed.todate'] = \
+        df_updated['tests.regular.performed'].fillna(0).cumsum().replace({0: None}).astype('Int64')
+    df_updated['tests.regular.positive.todate'] = \
+        df_updated['tests.regular.positive'].fillna(0).cumsum().replace({0: None}).astype('Int64')
+    df_updated['tests.hagt.performed.todate'] = \
+        df_updated['tests.hagt.performed'].fillna(0).cumsum().replace({0: None}).astype('Int64')
+    df_updated['tests.performed.todate'] = \
+        df_updated['tests.performed'].fillna(0).cumsum().replace({0: None}).astype('Int64')
+    df_updated['tests.positive.todate'] = \
+        df_updated['tests.positive'].fillna(0).cumsum().replace({0: None}).astype('Int64')
+
     print(df_updated)
 
     old_hash = sha1sum(filenameByDay)
