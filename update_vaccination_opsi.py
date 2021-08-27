735f3753b1ba47e67f176b5d0b5706da3bd6ecc3
Stefan Baebler <stefan.baebler@gmail.com>
2021-08-27 09:14:02 +0200

    Updated OPSI resource URL

--- update_vaccination_opsi.py
+++ update_vaccination_opsi.py
@@ -16,7 +16,7 @@ def saveurl(url, filename, expectedContentType):
 
 def import_opsi_vaccination_effects():
     # https://podatki.gov.si/dataset/potrjeni-primeri-covid-19-po-cepljenju
-    saveurl("https://podatki.gov.si/dataset/47edd697-a06b-44a6-86b7-0092ea34274a/resource/27e2b3d2-52b4-4899-be5c-9ee28a91bed5/download/pc14dnipozpublic.csv", "csv/vaccination-confirmed-cases-opsi.csv", "text/csv")
+    saveurl("https://podatki.gov.si/dataset/47edd697-a06b-44a6-86b7-0092ea34274a/resource/eb4bd862-737d-47f0-8e0a-47541986c2c3/download/pc14dnipozpublic.csv", "csv/vaccination-confirmed-cases-opsi.csv", "text/csv")
 
     # https://podatki.gov.si/dataset/hospitalizirani-primeri-sari-potrjeni-covid-19-po-cepilnem-statusu-v-sloveniji-nijz-cnb
     saveurl("https://podatki.gov.si/dataset/2a40d74b-4e75-4051-8ab7-289c70348cb7/resource/816af1be-4e56-4fca-a647-db7b09a2c98b/download/saricov19hospitalizacija.csv", "csv/vaccination-hospitalized-cases-opsi.csv", "text/csv")
