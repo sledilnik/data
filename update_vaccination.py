3b44d4da0d3be54b7706107c2d7acc1715acea92
Stefan Baebler <stefan.baebler@gmail.com>
2021-12-21 11:47:38 +0100

    Bump cepimose to 0.1.30; split 0-17 age group into 0-11 and 12-17

--- update_vaccination.py
+++ update_vaccination.py
@@ -214,7 +214,8 @@ def import_nijz_dash_vacc_by_age():
 
     # map cepimose regions to sledilnik regions, preserving previous order
     ageGroups = {
-        cepimose.data.AgeGroup.GROUP_0_17: "0-17",
+        cepimose.data.AgeGroup.GROUP_0_11: "0-11",
+        cepimose.data.AgeGroup.GROUP_12_17: "12-17",
         cepimose.data.AgeGroup.GROUP_18_24: "18-24",
         cepimose.data.AgeGroup.GROUP_25_29: "25-29",
         cepimose.data.AgeGroup.GROUP_30_34: "30-34",
